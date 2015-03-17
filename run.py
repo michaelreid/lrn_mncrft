009cff9b12d5bdeef38c1327a03abe6fa42dd592
m <m@m-2.local>
2015-03-17 20:44:09 +1030

    Compress images

--- run.py
+++ run.py
@@ -29,4 +29,4 @@ def about():
 
 # define __main__ function so can call from command line
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8085)
+    app.run(debug=True, host="0.0.0.0", port=8082)
